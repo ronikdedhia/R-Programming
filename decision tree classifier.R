0af3b996dd3cca1a33d07b333bf3f8a21ebec5de
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-10-08 20:29:23 +0530

    classification algorithm - decision tree on social network ads

---
+++ decision tree classifier.R
@@ -0,0 +1,26 @@
+dataset=read.csv("C:/Users/Acer/Documents/R/dataset/Social_Network_Ads.csv")
+dataset=dataset[,3:5]
+
+dataset$Purchased=factor(dataset$Purchased, levels=c(0,1))
+
+library(e1071)
+library(caTools)
+library(rpart)
+
+set.seed(1234)
+split=sample.split(dataset$Purchased, 0.75)
+
+training_set=subset(dataset, split==TRUE)
+test_set=subset(dataset, split==FALSE)
+
+training_set[,1:2]=scale(training_set[,1:2])
+test_set[,1:2]=scale(test_set[,1:2])
+
+classifier=rpart(formula=Purchased~.,
+                 data=training_set)
+
+y_pred = predict(classifier, newdata=test_set[-3], type='class')
+y_pred
+
+cm=table(test_set[,3], y_pred)
+cm
