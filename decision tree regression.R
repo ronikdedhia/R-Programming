dbdb599c7d64f19b949f943829c976daa31c6f82
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-10-09 15:37:30 +0530

    decision tree regression algorithm to define salaries

---
+++ decision tree regression.R
@@ -0,0 +1,23 @@
+dataset=read.csv("dataset/Position_Salaries.csv")
+dataset=dataset[2:3]
+
+library(rpart)
+
+regressor = rpart(formula=Salary~.,
+                data=dataset,
+                control=rpart.control(minsplit = 1))
+
+y_pred=predict(regressor, data.frame(Level=6.5))
+
+x_grid=seq(min(dataset$Level), max(dataset$Level), 0.01)
+
+ggplot() +
+  geom_point(aes(x=dataset$Level,
+                 y=dataset$Salary),
+             colour='red') +
+  geom_line(aes(x=x_grid,
+                y=predict(regressor, newdata=data.frame(Level = x_grid))),
+            colour='blue') +
+  ggtitle('Truth or Bluff') +
+  xlab('Level') +
+  ylab('Salary')
